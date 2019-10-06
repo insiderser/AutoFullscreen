74e04dfcee48dcfc350a34b38760d21b43b9a96f
Oleksandr Bezushko <34242332+insiderser@users.noreply.github.com>
2019-10-06 18:49:59 +0300

    Open all new windows in fullscreen mode.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,10 @@
 # Auto Fullscreen
 
-This is a dumb 10 lines or less plugin to give FireFox Quantum the ability to launch in Full Screen mode. On startup it simply updates the current window to set the window state to fullscreen. For best results set a homepage and configure Firefox to launch with that homepage on startup. Please create any issues on Github or submit a pull request for fixes. 
+This is a dumb 15 lines plugin that makes Firefox open all windows in fullscreen mode. 
+On startup it simply updates the current window to set the window state to fullscreen and adds 
+a listener to update subsequent new windows.
+
+This add-on is a fork. The original version can be found 
+[here](https://github.com/tazeat/AutoFullscreen).
+
+Found a bug? Please feel free to create any issues or submit a pull request.
--- startup.js
+++ startup.js
@@ -1,10 +1,24 @@
+/*
+ * Copyright (c) 2019 Oleksandr Bezushko
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 function onError(error) {
-	console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`);
 }
 
 console.log(`AutoFullscreen Running`);
 browser.windows.getAll().then((windowInfoArray) => {
-	for (currentWindow of windowInfoArray) {
-		browser.windows.update(currentWindow.id, {state: "fullscreen"});
-	}
+    for (currentWindow of windowInfoArray) {
+        browser.windows.update(currentWindow.id, {state: "fullscreen"});
+    }
 }, onError);
+
+browser.windows.onCreated.addListener((window) => {
+    console.log("New window: " + window.id);
+    browser.windows.update(window.id, {state: "fullscreen"});
+});
